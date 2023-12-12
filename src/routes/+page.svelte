3a5d42c975cc7ea6f5b2f38a0829d30062c210f2
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-12 21:29:33 +0100

    feat: add ordbokene and google translate links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,60 @@
 <script>
-	import {list} from './wordlist'
+	import {lista} from './wordlist-a'
+	import {listb} from './wordlist-b'
 
 	let randomElement = $state()
+  let currentLevel = $state('A')
+	let startButton = $derived(randomElement ? 'Next' : 'Start')
 
-	function randomword (){
-		randomElement = list[Math.floor(Math.random() * list.length)];
-		// console.log('random: ', randomElement) 
-		return randomElement
-	}
-
-	function openTab(word) {
-    const baseUrl = 'https://ordbokene.no/bm/search?q=';
+	function openTab(word, website) {
+		let baseUrl = '';
+		if( website === 'google'){
+			baseUrl = 'https://translate.google.com/?hl=en&tab=TT&sl=no&tl=en&op=translate&text='
+		} else {
+			baseUrl = 'https://ordbokene.no/bm/search?q=';
+		}
+	
     const url = baseUrl + encodeURIComponent(word);
     window.open(url, '_blank');
   }
+
+	function toggleLevel(){
+		currentLevel = currentLevel === 'A' ? 'B' : 'A';
+		randomElement = '';
+	}
+
+	function randomword (){
+		let selectedList = currentLevel === 'A' ? lista : listb;
+		randomElement = selectedList[Math.floor(Math.random() * selectedList.length)];
+		return randomElement;
+	}
 </script>
 
 <main class="m-8">
   <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Explain and Guess Norwegian Word</h1>
-	<p class="mb-6 text-xl font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Play this game with your friends to learn Norwegian.</p>
+	<p class="mb-6 text-xl font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Play this game with your friends or team to learn Norwegian.</p>
 	<h2 class="text-4xl font-bold dark:text-white">How to play</h2>
 	<ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 my-4">
-		<li>Player 1 click the button "Klikk" and explain the Norwegian word using Norwegian.</li>
-		<li>Player 2 guess the word or ask questions until you guess the word correctly.</li>
+		<li>Click "Change Level" to select your difficulty.</li>
+    <li>Player 1 clicks "Start" or continues clicking to find a Norwegian word to explain.</li>
+    <li>Player 2 guesses the word or asks questions in Norwegian until guessing correctly.</li>
+    <li>Take turns.</li>
 	</ul>
+
+	<button on:click={toggleLevel} class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800">
+		Change level: {currentLevel}
+	</button>
+	
 	<button on:click={randomword} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-		Klikk
+		{startButton}
 	</button>
 	<p class="my-4 text-2xl text-green-500">
-		Explain in Norwegian. If you need a hint, click the word.</p>
-	
-	<p class="my-4 text-2xl text-purple-500 hover:underline cursor-pointer"><span on:click={() => openTab(randomElement)}>{randomElement}</span>
+		Norwegian word: {randomElement}
+	</p>
+
+	<h2 class="text-4xl font-bold dark:text-white">Help</h2>
+	<p class="my-4 text-2xl text-purple-500 hover:underline cursor-pointer"><button  on:click={() => openTab(randomElement, 'ordbokene')}>Ordbøkene: {randomElement}</button>
+	</p>
+	<p class="my-4 text-2xl text-purple-500 hover:underline cursor-pointer"><button  on:click={() => openTab(randomElement, 'google')}>English translation: {randomElement}</button>
 	</p>
 </main>
---
+++ src/routes/wordlist-b.js
@@ -0,0 +1,61 @@
+export const listb = ["abonnere", "abort", "advare", "aksje", "aktivitet",
+  "allemannseie", "alt", "alternativ", "alvorlig", "amme", "analfabet",
+  "angre", "angripe", "anledning", "og", "annerledes", "ansatt", "ansvar",
+  "ansvarlig", "ansvarsfull", "antall", "arbeidsgiver", "arbeidskraft",
+  "arbeidsledig", "arbeidsom", "arbeidstaker", "arrangere", "arrestere",
+  "arv", "arve", "assistent", "asyl", "attraktiv", "autoritet", "autoritar",
+  "av og til", "av sted", "avbryte", "avdrag", "avfall", "avgjørelse",
+  "avgjørende", "avhengig av", "avstand", "avtale",
+
+  "bagatell", "bakgrunn", "balkong", "barnevakt", "barnever", "bedrift", "befolkning", "begrense", "begynne", "beholde", "behov", "og", "bekrefte", "bekymret", "bekymring", "beskjed", "beskjeden", "beskytte", "beskyttelse", "betraktelig", "bevare", "bevege esg", "bidra", "bidrag", "bitte smả", "blande seg i", "blid", "blind vold", "blodtrykk", "bolig", "boligblokk", "bombe", "borger", "bortskjemt", "bringe med seg", "brodere", "brutalitet", "bry seg om", "bryggeri", "bryte", "bryte ut", "bronn", "bud", "budsjett", "budskap", "busk", "by pả noe", "bygd", "byggeplass", "byrde", "bære", "bøtte",
+
+  "celle",
+  
+  "danne", "dekke utgiftene", "del", "delta", "dessuten", "dialekt", "diger", "dikte", "dikter", "dom", "dominerende", "domstol", "drabantby",
+  "drage", "dramatikk", "drepe", "drive med", "drommejobb", "dyrke", "dytte", "dømme", "døpe",
+  
+  "effektiv", "egenandel", "egenkapital", "egenskap", "egentlig", "eksistere", "eksotisk", "ekspedisjon", "ekspert", "eksplodere", "eksportere", "ekteskap", "emigrere", "en del", "en eller annen", "en kvart", "endelig", "endre seg", "engasjere seg", "engstelig", "enig", "enkelte", "entré", "erfaring", "erklaring", "erobre", "erstatning", "erte", "etablere", "etnisk", "etterlyse", "eventyr", "evighet", "evne",
+  
+  "fabrikk", "fagbrev", "fagprøve", "faktisk", "fale utenfor", "falsk", "fange", "fangeleir", "farmasøyt", "fast ansatt", "fastlege", "feire", "feiring", "felles", "fellesgjeld", "felleskostnad", "fellesskap", "fengsel", "ferd", "ferdigprodusert", "fiende", "finne ut", "finnes", "firma", "fiskeoppdrett", "fiskeslag", "fjerdedel", "fjøs", "fly", "flykte", "flyktning", "flåte", "fond", "for det meste", "forslag", "for øvrig", "forandre seg", "forarbeid", "forberede", "forberedelse", "forbilde", "forbruk", "forbudt", "fordel", "fordele", "forekomst", "forelopig", "forestilling", "foretrekke", "forfolgelse", "forgårs", "forhandle", "forhold", "og", "forhold", "forlate", "forme", "fornuftig", "fornybar", "fornøyd", "forsker", "forskrekket", "forsvare", "forsvinne", "fortelling", "fortjene", "forurensning", "forvalte",
+  "frakte", "framfor alt", "framtre", "franchisetaker", "fred", "fred", "fremdeles", "friidrett", "friminutt", "frisør", "fritid", "frustrert", "frykt", "fryse", "fundament", "fungere", "fæl", "færre", "fødsel", "følge med", "føre til", "først og fremst", "få frem", "få vite",
+  
+  "gal", "gammeldags", "generasjon", "geologi", "gi opp", "gjeld", "gjelde", "giemme seg", "gjennomfore", "gjennomsnitt", "gientakelse", "gjest", "glad", "glede seg til noe", "glimt", "gni", "godkjenne", "godkjenning", "godteri", "gratis", "gravid", "greit", "grunnen til", "grunnlov", "gryte", "grøt", "gøy", "gå iarv", "gå inn for noe",
+  
+  "ha bruk for", "ha kontakt med", "ha lyst til", "ha lyst på", "halvpart", "handel", "handelsmann", "havbruk", "hedre", "heis", "helsesektor", "helsetjeneste", "helt", "hes", "hjel, se i hjel", "hjelp", "hjemmeværende", "hogge trar", "holde på", "holde på med", "holde på å", "holde seg for seg selv", "holde seg hjemme", "holde seg unna", "holdning", "homogen", "hopptrener", "hovedperson", "husdyr", "hushjelp", "høflig", "høre til", "høytidelg", "håndtrykk", "håndverk", "håndverker", "håpløs",
+  
+  "i alle fall", "i alt", "i hjel", "ihjel", "i likhet med", "i løpet av dagen", "i stedet for", "i tillegg", "ide", "ifølge", "ille", "illegal", "imidlertid", "indre", "inkludere", "innflytelse", "innflyter", "innfore", "inngrep", "innkalle (til intervju)", "innromme", "intekt", "innvandrer", "inspirere", "integrert", "internasjonal", "interju", "intervjue", "intim", "investere", "involvere seg", "irritert", "i stedet for",
+  
+  "jakt", "jammen", "jordbruk", "jordbruksområde", "journalist",
+  
+  "kamp", "kant", "kantine", "karakterisere", "karneval", "karriere", "kaste bort tid", "katastrofal", "katastrofe", "keiser", "kilde", "kildesortere", "kjede seg",
+  "kjedelig", "kjempe for", "kjendis", "kjenne til", "kjennskap", "kjent", "kjonnsrolle", "klar", "klar", "klare", "klare seg", "Kle seg ut", "klemme", "klima", "klimapanel", "kloster", "knapt nok", "knuse", "knyttet til", "kollega", "kollisjonsfare", "kommandere", "komme igang", "komme inn i", "komme innom", "kompetent", "komplisert", "konferanse", "konflikt", "konsekvens", "konstitusjon", "konsulent", "kontakt", "kontaktnett", "konto", "konvensjon", "konvensjonell", "kor", "kortsiktig", "kosthold", "kostyme", "kraft", "kraftig", "kreft", "kreve", "krigstilstand", "kritisere", "kronisk", "kronisk syk", "kultur", "kunde", "kunnskap", "kvalifikasjon", "kvart", "kåre",
+  
+  "lader", "lager", "landbruk", "landskap", "late som", "ledighet", "legge merke til", "legge press på", "legge på seg", "legge til rette", "legge ut (pa nettet)", "legge vekt på", "legning", "leie", "lettet", "leve i skjul", "likestilling", "likeverd", "litt etter litt", "livssyn", "lokke", "lov", "lovlost", "lukt", "lur", "lure på", "lykke", "lyst", "lærling", "løft", "løfte", "lønn", "løse", "lån",
+  
+  "majoritet", "makt", "mangel", "mannskap", "manøvrere", "maraton", "mar kere", "mas", "massevis", "medlem", "mekanisere", "mekanisk", "melde", "melde seg inn", "menneskerettighet", "menneskerettighets- konvensjon", "mental", "militær", "miljøorganisasjon", "miljøvennlig", "miljøvern", "minne om", "minoritet", "mislykket", "mistake", "mobbing", "moden", "modig", "mosjon", "mosjonere", "moské", "motiv", "motsetning", "motstand", "motstander", "motvirke", "munter", "muslim", "myndighetene", "mønster", "mål", "målbevisst", "måling",
+  
+  "naboland", "nazist", "nekte", "nervøs", "nettverk", "nifst", "nisse", "nok", "nyfødt", "nysgierrig", "nytte", "nyttig", "næring", "nærme seg", "nød", "nødt", "nødvendig", "nøle", "nå til dags",
+  
+  "offensiv", "offentlig", "omfatte", "omkring", "område", "omsetning", "omsorgssvikt", "omtrent", "omvendt", "oppdage", "oppdagelsesreisende", "oppdat ert", "oppdra", "oppfordre", "oppfølging", "oppgiør", "oppholdstillatelse", "oppleve", "oppløse", "oppnå", "opprette", "opprør", "oppslag", "opptak", "opptatt av", "opptog", "opptøyer", "oppvekst", "ordne", "ordne seg", "organisere", "orke", "overdrive", "overfall", "overfalle", "overgrep", "overleve", "overnaturlig", "overrasket", "overta", "overtale", "overvekt", "overvektig",
+  
+  "pante flasker", "parallellklasse", "pave", "pensjonist", "periode", "permanent", "permisjon", "personvern", "plage (s)", "plage (v)", "plan", "planlegge", "plante", "plassere", "pleie", "pleieassistent", "plen", "plikt", "plyndre", "polfarer", "post", "preg", "prest", "prisantydning", "produkt", "publisere", "pugging", "pusse opp", "pynte seg", "på egen hånd", "på tide", "påstå", "påvirke", "påvirkning",
+  
+  "ransmann", "rapport", "raskt", "reagere", "reaksjon", "realistisk", "redaktør", "redsel", "redusere", "regel", "regel", "registrere", "regjering", "regning", "regulere", "reise seg", "relativt", "relevant", "rente", "reservere", "respekt", "ressurs", "rett til", "rett", "rette", "rettighet", "revolusjon", "risiko", "ros", "rose (v)", "rydde", "ryddig", "rødme", "rømme", "råd", "rådgiver",
+  
+  "sabotasje", "sage", "samarbeid", "samboer", "samfunn", "samhold", "samle", "sammenheng", "samvittighet", "satse på", "satsing", "se ... ut", "selvbestemt", "selvrespekt", "selvstendig", "selvstendighet", "senke temperaturen", "sette fra seg", "sette igang", "sette inn",
+  "sette pris på", "sette sammen", "sette seg inn i", "si opp noen", "siden", "sikker pa", "sikkerhet", "sikre", "siste halvdel av", "siste nytt", "sjikane", "sjåfør", "skade", "skaffe seg", "skam", "skandale", "skape", "skatt", "skeptisk", "skifte", "skikk", "skikkelig", "skille seg ut", "skilsmisse", "skilt", "skjema", "skjule", "skogbruk", "skremme", "skremme noen", "skrike", "skryt", "skryte", "skuespill", "skuffet", "slagord", "slapp", "slave", "slede", "slett ikke", "slippe", "slippe noen til", "slite", "slå seg ned", "snike seg", "snorke", "som regel", "sosial", "sovne", "spandere", "spare", "sparke fotball", "spennende", "spre", "spøk", "stabil", "stadig", "stagnere", "stamme", "starte for seg selv", "statskirke", "statsminister", "statsråd", "status", "stave", "sterk", "stifte", "stilling", "stimulere", "Stortinget", "straff", "straffbar", "strafferamme", "strøm", "stråling",
+  "stykke", "støtte", "stå for noe", "stå på", "stå på sitt", "sukke", "suksess", "sunn", "supert", "sur", "surfe på nettet", "svak", "svekke", "sykehjem", "symbol", "symbolisere", "systematisere", "systematisk", "søknad", "sørge for", "sørgelig", "søvnig", "sårbar",
+  
+  "ta opp", "ta opp lån", "ta seg av noen", "ta tid", "ta vare på", "tabbe seg ut", "talentfull", "tape", "tatovering", "tekstil", "telt", "tenne", "tenåring", "teori", "test", "tid", "til tross for", "tilbud", "tilby", "tilfeldig", "tilfelle", "tilknytning", "tillegg", "tilpasse seg", "tilretelegge", "tilsette", "tilstand", "tiår", "tjene", "toleranse", "tolerere", "torg", "torpedo", "tradisjon", "tradisjonell", "trappe ned", "treffes", "trekke fra", "trene", "trenge", "trives", "tro", "trossamfunn", "true", "trygd", "trygghet", "trøste", "tulle", "tung tid",
+  "tvinge", "tydeligvis", "typisk", "tømmer", "tørket fisk",
+  
+  "uakseptabel", "uansett", "uavhengig av", "ufaglart", "uforberedt", "ufør", "ukonsentrert", "underlegen", "undertrykke", "underveis", "union", "unna", "unngå", "unådvendig", "uro", "urolighet", "uskolert", "utenat", "utenlandsk", "utestenging", "utfordring", "utgift", "utgjøre", "utkant", "utmattelse", "utnevne", "utrolig", "utsatt for", "utslitt", "uttrykk", "uttrykk", "utvikle", "utvikle seg", "utvinning", "utøver", "uvilje",
+  
+  "valg", "vant til", "vare", "vare", "variere", "ved", "vedta", "vekt", "velferdsstat", "velge", "velstående", "verdensbasis", "verdifull", "verksted", "verktøy", "verst", "vesen", "via", "videre", "vikar", "vikariat", "viktig", "virke", "visning", "visstnok", "vits", "vold", "voldta", "vurdere", "være glad", "være med på", "være nodt til", "være vant til", "våge",
+  
+  "ytre", "ytringsfrihet",
+  
+  "ære",
+  
+  "ødelegge", "øke", "øse", "østover", "øve seg"
+];
