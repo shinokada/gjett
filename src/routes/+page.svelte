8495c27dc99e04e6a51efcf2b018887ca2d931ce
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-13 22:10:29 +0100

    feat: add bg image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,8 +30,8 @@
 	}
 </script>
 
-<section class="bg-white dark:bg-gray-900">
-	<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+<section class="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+	<div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
 		<h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Gjett Norsk</h1>
 		<p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Learn Norwegian together with friends through interactive app, making learning enjoyable and collaborative.</p>
 		<h2 class="text-center sm:text-left text-3xl font-bold mb-4 dark:text-white">How to play</h2>
@@ -79,26 +79,29 @@
 		</div>
 		{/if}
 	</div>
+	<div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+
+	<footer class="bg-white dark:bg-gray-900 m-4 border-t">
+		<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+			<div class="sm:flex sm:items-center sm:justify-between">
+					<a href="https://gjett-norsk.vercel.app/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+						<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Gjett Norsk</span>
+					</a>
+					<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+						<li>
+								<a href="https://svelte-5-preview.vercel.app/docs/introduction" target="_blank" class="hover:underline me-4 md:me-6">Svelte 5</a>
+						</li>
+						<li>
+								<a href="https://github.com/shinokada/gjett-norsk" target="_blank" class="hover:underline me-4 md:me-6">Github repo</a>
+						</li>
+						<li>
+								<a href="https://github.com/shinokada" target="_blank" class="hover:underline">Made by Shin</a>
+						</li>
+					</ul>
+			</div>
+		</div>
+	</footer>
 </section>
 
-<footer class="bg-white dark:bg-gray-900 m-4 border-t">
-	<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-		<div class="sm:flex sm:items-center sm:justify-between">
-				<a href="https://gjett-norsk.vercel.app/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Gjett Norsk</span>
-				</a>
-				<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-					<li>
-							<a href="https://svelte-5-preview.vercel.app/docs/introduction" target="_blank" class="hover:underline me-4 md:me-6">Svelte 5</a>
-					</li>
-					<li>
-							<a href="https://github.com/shinokada/gjett-norsk" target="_blank" class="hover:underline me-4 md:me-6">Github repo</a>
-					</li>
-					<li>
-							<a href="https://github.com/shinokada" target="_blank" class="hover:underline">Made by Shin</a>
-					</li>
-				</ul>
-		</div>
-	</div>
-</footer>
+
 
