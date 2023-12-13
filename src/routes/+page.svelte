602398a839f3c267c5937231b49e046460e3444a
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-13 21:35:32 +0100

    feat: add footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -80,3 +80,25 @@
 		{/if}
 	</div>
 </section>
+
+<footer class="bg-white dark:bg-gray-900 m-4 border-t">
+	<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+		<div class="sm:flex sm:items-center sm:justify-between">
+				<a href="https://gjett-norsk.vercel.app/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Gjett Norsk</span>
+				</a>
+				<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+					<li>
+							<a href="https://svelte-5-preview.vercel.app/docs/introduction" target="_blank" class="hover:underline me-4 md:me-6">Svelte 5</a>
+					</li>
+					<li>
+							<a href="https://github.com/shinokada/gjett-norsk" target="_blank" class="hover:underline me-4 md:me-6">Github repo</a>
+					</li>
+					<li>
+							<a href="https://github.com/shinokada" target="_blank" class="hover:underline">Made by Shin</a>
+					</li>
+				</ul>
+		</div>
+	</div>
+</footer>
+
