5ae6df289b73980cab463c90e757c0068d72938e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-14 16:50:31 +0100

    fix: add z-10 to ul and footer span

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -81,15 +81,14 @@
 		</div>
 		{/if}
 	</div>
-	<div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
 
 	<footer class="bg-white dark:bg-gray-900 m-4 border-t">
 		<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 			<div class="sm:flex sm:items-center sm:justify-between">
-					<a href="https://gjett-norsk.vercel.app/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+					<a href="https://gjett-norsk.vercel.app/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse z-10">
 						<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Gjett Norsk</span>
 					</a>
-					<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+					<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 z-10">
 						<li>
 								<a href="https://svelte-5-preview.vercel.app/docs/introduction" target="_blank" class="hover:underline me-4 md:me-6">Svelte 5</a>
 						</li>
@@ -103,6 +102,7 @@
 			</div>
 		</div>
 	</footer>
+	<div class="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
 </section>
 
 
