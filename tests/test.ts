4959d7e87cf7debde22aebcb92ce4a1696c7e008
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-03 16:34:31 +0100

    test: add tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,32 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	expect(await page.textContent('h1')).toBe('Nivå A1/A2');
 });
+
+test('a1 page has expected h1', async ({ page }) => {
+	await page.goto('/a1');
+	expect(await page.textContent('h1')).toBe('Nivå A1');
+});
+
+test('a2 page has expected h1', async ({ page }) => {
+	await page.goto('/a2');
+	expect(await page.textContent('h1')).toBe('Nivå A2');
+});
+
+test('b page has expected h1', async ({ page }) => {
+	await page.goto('/b');
+	expect(await page.textContent('h1')).toBe('Nivå B1/B2');
+});
+
+test('how to play page has expected h1', async ({ page }) => {
+	await page.goto('/how-to-play');
+	expect(await page.textContent('h1')).toBe('How to play');
+});
+
+test('credits page has expected h1', async ({ page }) => {
+	await page.goto('/credits');
+	expect(await page.textContent('h1')).toBe('Credits');
+});
+
+
