ab62905aa3de2f4e7fdd80400344deffe478a26b
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-02-14 21:46:20 +0100

    docs: move how to play to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,7 +7,28 @@
 
 <h1 class="text-left text-3xl m-4 dark:text-white">About</h1>
 
-<h2 class="text-left text-2xl m-4 font-bold dark:text-white">LexiLearn Hub apps</h2>
+<h2 class="text-left text-2xl m-4 font-bold dark:text-white">How to play</h2>
+
+<ul class="text-left space-y-1 m-4 text-gray-500 list-inside dark:text-gray-400">
+  <li class="flex items-center">
+    <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+   </svg> Select your difficulty from the main menu.</li>
+  <li class="flex items-center">
+    <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+   </svg> Player 1 clicks "Start" or continues clicking to find a Norwegian word to explain. Explain the word in Norwegian.</li>
+  <li class="flex items-center">
+    <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+   </svg> Player 2 guesses the word or asks questions until guessing correctly.</li>
+  <li class="flex items-center">
+    <svg class="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+   </svg> Take turns.</li>
+</ul>
+
+<h2 class="text-left text-2xl m-4 font-bold dark:text-white">Another LexiLearn Hub apps</h2>
 
 <ul class="text-left m-4 p-4 text-lg list-disc dark:text-gray-400">
   <li class="hover:text-red-700 hover:underline">
@@ -27,7 +48,7 @@
   </li>
 </ul>
 
-<h2 class="text-left text-2xl m-4 font-bold dark:text-white">Stack info</h2>
+<h2 class="text-left text-2xl m-4 font-bold dark:text-white">Technical info</h2>
 <ul class="text-left m-4 p-4 text-lg list-disc dark:text-gray-400">
   <li class="hover:text-red-700 hover:underline">
     <a href="https://svelte-5-preview.vercel.app/docs/introduction" class="hover:underline me-4 md:me-6">Svelte 5: { svelteVersion }</a>
--- tests/test.ts
+++ tests/test.ts
@@ -20,11 +20,6 @@ test('b page has expected h1', async ({ page }) => {
 	expect(await page.textContent('h1')).toBe('Nivå B1/B2');
 });
 
-test('how to play page has expected h1', async ({ page }) => {
-	await page.goto('/how-to-play');
-	expect(await page.textContent('h1')).toBe('How to play');
-});
-
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
 	expect(await page.textContent('h1')).toBe('About');
