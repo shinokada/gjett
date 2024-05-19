229a82f49f715d30ee3619494a2a31cb47ae9759
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-19 20:44:04 +0200

    fix: env key name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
     metaTags = $page.data.pageMetaTags ? deepMerge($page.data.layoutMetaTags, $page.data.pageMetaTags ) : data.layoutMetaTags
   });
 
-  const analyticsId = data.ANALYTICS_ID
+  const analyticsId = data.ANALYTICS_ID_LANGUAGE_APP
 </script>
 
 <Runatics {analyticsId} />
